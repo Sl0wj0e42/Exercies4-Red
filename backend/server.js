53d0679ca09b064ab6679c39e27b10ed543c3022
(s) Joseph Malone <jmalone1@SOCNET.SOC.PLYMOUTH.AC.UK>
2024-11-11 12:51:29 +0000

    Red works

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,13 @@ let server = http.createServer(function(request, response){
     response.writeHead(200, {"Access-Control-Allow-Origin": "*"});
     let urlParts = url.parse(request.url, true);
     if(urlParts.path == "/red"){
-        response.end("<p>Here is some"
-            + "<span style=\"color:red;\"> red </span> text from the back end</p>");
-    } else if(urlParts.path == "/blue"){
+        response.end("<p>Here is some <span style=\"color:red;\">red</span> text from the back end</p>");
+    /*} else if(urlParts.path == "/blue"){
         response.end("<p>Here is some" 
             + "<span style=\"color: blue;\">blue<\span> text <\p>");
     } else if(urlParts.path == "/green"){
         response.end("<p>Here is some" 
-            + "<span style=\"color: green;\">green<\span> text <\p>");
+            + "<span style=\"color: green;\">green<\span> text <\p>");*/
     }else {
         response.end("<p>This is some plain text</p>");
     }
--- red/colourRedJs.js
+++ red/colourRedJs.js
@@ -1,30 +1,33 @@
-/*window.onload = function(){
+window.onload = function(){
     alert("Red!!!");
-    let urlHolder = getCurrentURL()
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(this.readyState === 4 && this.status == 200) {
-            let elem = document.getElementById("colourSetting");
-            elem.innerHTML = this.responseText;
-        } else{
-            elem = document.getElementById("colourSetting");
-            elem.innerHTML = this.status;
-        }
-    };
-    request.open("GET", "http://localhost:82/", true);
-    request.send();
+    //let urlHolder = getCurrentURL();
 
+    let urlHolder = window.location.href;
     urlShow = document.getElementById("urlShow");
     urlShow.innerHTML = urlHolder;
-}*/
 
-window.onload = function(){
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if((this.readyState === 4) && (this.status == 200)) {
+            let colourSetting = document.getElementById("colourSetting");
+            colourSetting.innerHTML = this.responseText;
+        } else{
+            colourSetting = document.getElementById("colourSetting");
+            colourSetting.innerHTML = "Error: " + this.status;
+        }
+    };
+    request.open("GET", "http://localhost:82/red", true);
+    request.send();
+}
+
+
+/*window.onload = function(){
     alert("Red!!!");
     var colourSetting = document.getElementById("colourSetting");
     pColour = document.createElement("p");
-    pColour.innerHTML = "<p>Here is some" + "<span style=\"color: red;\"> red<\span> text <\p>";
+    pColour.innerHTML = "<p>Here is some <span style=\"color: red;\"> red<\span> text <\p>";
     colourSetting.append(pColour);
 
     //let test = document.getElementById("test");
     //test.innerHTML = "<p>Here is some" + "<span style=\"color: red;\"> red<\span> text <\p>";
-}
+}*/
